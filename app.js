8ba7c26a3f9d8a0988521e6bc9d3dbb3568b1265
Ryan Hantak <rhantak@gmail.com>
2020-01-25 13:40:32 -0700

    Complete express knex practice exercise

---
+++ app.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ entended: true }));
+app.set('port', process.env.PORT || 3000);
+app.locals.title = 'Publications';
+
+app.get('/', (request, response) => {
+  response.send('Hello, Publications');
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+});
+
+app.get('/api/v1/papers', (request, response) => {
+  database('papers').select()
+    .then((papers) => {
+      response.status(200).json(papers);
+    })
+    .catch((error) => {
+      response.status(500).json({ error });
+    });
+});
+
+app.get('/api/v1/footnotes', (request, response) => {
+  database('footnotes').select()
+    .then((footnotes) => {
+      response.status(200).json(footnotes);
+    })
+    .catch((error) => {
+      response.status(500).json({ error });
+    });
+});
+
+app.get('/api/v1/papers/:id', (request, response) => {
+  database('papers').where('id', request.params.id).select()
+    .then(papers => {
+      if (papers.length) {
+        response.status(200).json(papers);
+      } else {
+        response.status(404).json({
+          error: `Could not find paper with id ${request.params.id}`
+        });
+      }
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+});
+
+app.get('/api/v1/papers/:id/footnotes', (request, response) => {
+  database('footnotes').where('paper_id', request.params.id).select()
+    .then(footnotes => {
+      if (footnotes.length) {
+        response.status(200).json(footnotes);
+      } else {
+        response.status(404).json({
+          error: `Could not find footnotes for paper with id ${request.params.id}`
+        });
+      }
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+});
